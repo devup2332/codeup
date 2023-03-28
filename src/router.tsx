6738c9d888dc387f0ac117e763e9ef4292c2c67b
Diego Rojas <devup2332@gmail.com>
2023-03-27 20:21:52 -0500

    dev: Added home page

--- src/router.tsx
+++ src/router.tsx
@@ -1,8 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
+import HomePage from "./pages/homepage";
 import LoginPage from "./pages/login";
 import RegisterPage from "./pages/register";
 
 export const router = createBrowserRouter([
+  {
+    path: "",
+    Component: HomePage,
+  },
   {
     path: "login",
     Component: LoginPage,
